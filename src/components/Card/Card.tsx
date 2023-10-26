ebc34abf38b7d5273d3dfe2c7263442f14d92bb4
oneren37-1 <oneren38@gmail.com>
2023-10-26 18:23:23 +0300

    card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import style from './Card.module.scss'
+import Headline from "../Typography/Headline/Headline";
+import Text from "../Typography/Text/Text";
+import Caption from "../Typography/Caption/Caption";
+import Icon from "../Icon/Icon";
+import {faEllipsisVertical} from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+import {usePlatform} from "../../app/hooks";
+
+export interface ICardProps {
+    username: string;
+    avatar: string;
+    company: string;
+    location: string;
+    disabled?: boolean;
+}
+
+const Card = (props: ICardProps) => {
+    const platform = usePlatform();
+
+    return (
+        <div className={classNames(
+            style.card,
+            { [style.mobile]: platform === 'mobile' },
+            { [style.disabled]: props.disabled }
+        )}>
+            <img className={style.avatar} src={props.avatar} alt={props.username} />
+            <div className={style.contentWrapper}>
+                <div className={style.info}>
+                    <div className={style.content}>
+                        <Headline className={style.username}>{props.username}</Headline>
+                        <Text>{props.company}</Text>
+                    </div>
+                    <div className={style.footer}>
+                        <Caption className={style.location}>{props.location}</Caption>
+                    </div>
+                </div>
+                <div className={style.menu}>
+                    <Icon i={faEllipsisVertical}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {Platform, PlatformContext} from "../components/PlatformContext/PlatformContext";
+import {ComponentProps} from "react";
+import Card from "../components/Card/Card";
+
+const meta = {
+    title: 'Card',
+    component: Card,
+    decorators: [
+        (Story, { args }) => (
+            <PlatformContext.Provider value={args.platformContextValue}>
+                <div style={{padding: "100px", background: "#eee"}} >
+                    <Story />
+                </div>
+            </PlatformContext.Provider>
+        ),
+    ],
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        platformContextValue: { control: 'inline-radio', options: ['desktop', 'mobile'] },
+    },
+} satisfies Meta< ComponentProps<typeof Card> & { platformContextValue: Platform }>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Desktop: Story = {
+    args: {
+        username: "Ivan1234",
+        avatar: "https://s3-alpha-sig.figma.com/img/4d47/8bb4/c4df08734f93921230736e92b21788b4?Expires=1699228800&Signature=kLX9Bc5Nx4797G3wkRf1ANUssBLk7KBauB1Qgh~vlSRncgGbu7d2lkKMkHELvckNp4dv0hDwQraf2Gw1rC7UAXAFjWPS9JGGKAzXPdxL26xlFGOph94larjoBXAWqmjWi9k5WwAZC7DOAb7-npVco5u2VP83Xl6hxD~UndS9tnHPcF-hECMwoOKCafGMkTFRe7fmhHxqxtIeExl-7Xek~kpw4d8RxXDU~82JeMzP9L4IWR1q-7kdmLRxdEZ7zIB1-k70b07sGjVknYSlCjjGg3BBTccNui2Q5NE-njtOv7q62zlJk4YFvoKVrSJWRMuz9hI1XzqpO-rhFoaVOPHjIg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        company: "At-Work",
+        location: "Санкт-Петербург",
+        platformContextValue: 'desktop',
+    },
+};
+
+export const Mobile: Story = {
+    args: {
+        username: "Ivan1234",
+        avatar: "https://s3-alpha-sig.figma.com/img/4d47/8bb4/c4df08734f93921230736e92b21788b4?Expires=1699228800&Signature=kLX9Bc5Nx4797G3wkRf1ANUssBLk7KBauB1Qgh~vlSRncgGbu7d2lkKMkHELvckNp4dv0hDwQraf2Gw1rC7UAXAFjWPS9JGGKAzXPdxL26xlFGOph94larjoBXAWqmjWi9k5WwAZC7DOAb7-npVco5u2VP83Xl6hxD~UndS9tnHPcF-hECMwoOKCafGMkTFRe7fmhHxqxtIeExl-7Xek~kpw4d8RxXDU~82JeMzP9L4IWR1q-7kdmLRxdEZ7zIB1-k70b07sGjVknYSlCjjGg3BBTccNui2Q5NE-njtOv7q62zlJk4YFvoKVrSJWRMuz9hI1XzqpO-rhFoaVOPHjIg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        company: "At-Work",
+        location: "Санкт-Петербург",
+        platformContextValue: 'mobile',
+    },
+};
+
+export const Disabled: Story = {
+    args: {
+        username: "Ivan1234",
+        avatar: "https://s3-alpha-sig.figma.com/img/4d47/8bb4/c4df08734f93921230736e92b21788b4?Expires=1699228800&Signature=kLX9Bc5Nx4797G3wkRf1ANUssBLk7KBauB1Qgh~vlSRncgGbu7d2lkKMkHELvckNp4dv0hDwQraf2Gw1rC7UAXAFjWPS9JGGKAzXPdxL26xlFGOph94larjoBXAWqmjWi9k5WwAZC7DOAb7-npVco5u2VP83Xl6hxD~UndS9tnHPcF-hECMwoOKCafGMkTFRe7fmhHxqxtIeExl-7Xek~kpw4d8RxXDU~82JeMzP9L4IWR1q-7kdmLRxdEZ7zIB1-k70b07sGjVknYSlCjjGg3BBTccNui2Q5NE-njtOv7q62zlJk4YFvoKVrSJWRMuz9hI1XzqpO-rhFoaVOPHjIg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        company: "At-Work",
+        location: "Санкт-Петербург",
+        disabled: true,
+        platformContextValue: 'desktop',
+    },
+};
